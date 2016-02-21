1b0023a898cc388ec768e2944e69a6ed822fdb27
Franiac <d.rebitzer@gmx.de>
2016-02-21 02:36:34 +0100

    Separate AssamblyInfo for each assembly

--- TwitchLeecher.Setup/GlobalAssemblyInfo.cs
+++ TwitchLeecher.Setup/GlobalAssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Twitch Leecher Installer")]
+[assembly: AssemblyDescription("Installer for Twitch Leecher Broadcast Downloader")]
 [assembly: AssemblyCompany("Fake Smile Revolution")]
 [assembly: AssemblyCopyright("Copyright © 2016 Dominik Rebitzer")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- TwitchLeecher.Setup/TwitchLeecher.Setup.Gui/Properties/AssemblyInfo.cs
+++ TwitchLeecher.Setup/TwitchLeecher.Setup.Gui/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("TwitchLeecher.Setup.Gui")]
-[assembly: AssemblyProductAttribute("Framework Studio")]
-[assembly: AssemblyCompanyAttribute("Framework Systems GmbH")]
-[assembly: AssemblyCopyrightAttribute("Copyright © 2015")]
-[assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: BootstrapperApplication(typeof(TwitchLeecher.Setup.Gui.SetupApplication))]
