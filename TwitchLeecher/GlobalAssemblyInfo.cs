d00d8ec9b68254a992af6420992a606f86d3fb7f
Franiac <d.rebitzer@gmx.de>
2016-06-15 19:11:00 +0200

    Release - Twitch Leecher 1.2.2

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Fake Smile Revolution")]
 [assembly: AssemblyCopyright("Copyright © 2016 Dominik Rebitzer")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
