34aa53c24812635714f99eed4ccea92fc66f431a
Franiac <d.rebitzer@gmx.de>
2019-07-28 21:10:09 +0200

    use OnBeforeBrowse to determine auth redirect url

---
+++ TwitchLeecher/TwitchLeecher.Gui/Browser/AuthRequestHandler.cs
@@ -0,0 +1,46 @@
+﻿using CefSharp;
+using CefSharp.Handler;
+using System;
+using System.Windows.Threading;
+using TwitchLeecher.Gui.ViewModels;
+
+namespace TwitchLeecher.Gui.Browser
+{
+    internal class AuthRequestHandler : DefaultRequestHandler
+    {
+        private Dispatcher _dispatcher;
+        private TwitchConnectViewVM _vm;
+
+        public AuthRequestHandler(Dispatcher dispatcher, TwitchConnectViewVM vm)
+        {
+            _dispatcher = dispatcher;
+            _vm = vm;
+        }
+
+        public TwitchConnectViewVM DataContext { get; private set; }
+
+        public override bool OnBeforeBrowse(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, bool userGesture, bool isRedirect)
+        {
+            string urlStr = request.Url;
+
+            if (!string.IsNullOrWhiteSpace(urlStr) && urlStr.StartsWith("http://www.tl.com", StringComparison.OrdinalIgnoreCase) && Uri.TryCreate(urlStr, UriKind.Absolute, out Uri accessUri))
+            {
+                void Navigate(Uri uri)
+                {
+                    _vm?.NavigatingCommand.Execute(uri);
+                }
+
+                if (!_dispatcher.CheckAccess())
+                {
+                    _dispatcher.Invoke(() => Navigate(accessUri));
+                }
+                else
+                {
+                    Navigate(accessUri);
+                }
+            }
+
+            return base.OnBeforeBrowse(browserControl, browser, frame, request, userGesture, isRedirect);
+        }
+    }
+}
--- TwitchLeecher/TwitchLeecher.Gui/Views/TwitchConnectView.xaml.cs
+++ TwitchLeecher/TwitchLeecher.Gui/Views/TwitchConnectView.xaml.cs
@@ -1,7 +1,6 @@
-﻿using CefSharp;
-using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using TwitchLeecher.Gui.Browser;
 using TwitchLeecher.Gui.ViewModels;
 
 namespace TwitchLeecher.Gui.Views
@@ -19,7 +18,7 @@ namespace TwitchLeecher.Gui.Views
         private void TwitchConnectView_Loaded(object sender, RoutedEventArgs e)
         {
             chrome.IsBrowserInitializedChanged += Chrome_IsBrowserInitializedChanged;
-            chrome.LoadError += Chrome_LoadError;
+            chrome.RequestHandler = new AuthRequestHandler(Dispatcher, DataContext as TwitchConnectViewVM);
         }
 
         private void TwitchConnectView_Unloaded(object sender, RoutedEventArgs e)
@@ -32,23 +31,6 @@ namespace TwitchLeecher.Gui.Views
             if (((bool)e.NewValue) == true)
             {
                 chrome.Load("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=37v97169hnj8kaoq8fs3hzz8v6jezdj&redirect_uri=http://www.tl.com&scope=user_subscriptions&force_verify=true");
-            }
-        }
-
-        private void Chrome_LoadError(object sender, LoadErrorEventArgs e)
-        {
-            string urlStr = e.FailedUrl;
-
-            if (!string.IsNullOrWhiteSpace(urlStr) && urlStr.StartsWith("http://www.tl.com", StringComparison.OrdinalIgnoreCase) && Uri.TryCreate(urlStr, UriKind.Absolute, out Uri accessUri))
-            {
-                if (!Dispatcher.CheckAccess())
-                {
-                    Dispatcher.Invoke(() => Chrome_LoadError(sender, e));
-                    return;
-                }
-
-                TwitchConnectViewVM vm = DataContext as TwitchConnectViewVM;
-                vm?.NavigatingCommand.Execute(accessUri);
             }
         }
     }
