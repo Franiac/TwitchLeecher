584f31351a7c5260e2bffd64f3bb414457fd94cb
Franiac <d.rebitzer@gmx.de>
2021-03-06 20:52:00 +0100

    version bump to 1.8.4

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Franiac")]
 [assembly: AssemblyCopyright("Copyright © 2021 Dominik Rebitzer")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.8.3")]
+[assembly: AssemblyFileVersion("1.8.4")]
 [assembly: ComVisible(false)]
