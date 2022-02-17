cc1f15d9b114c6dae3a05f30330f50a9ccfcc44c
Dominik Rebitzer <d.rebitzer@gmx.de>
2022-02-18 00:08:47 +0100

    bump version to 2.0.4

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Franiac")]
 [assembly: AssemblyCopyright("Copyright © 2021 Dominik Rebitzer")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.3")]
+[assembly: AssemblyFileVersion("2.0.4")]
 [assembly: ComVisible(false)]
