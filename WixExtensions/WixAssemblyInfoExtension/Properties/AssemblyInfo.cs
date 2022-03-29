76155a27e58f0fcd2e812b833af24f81cd256137
Dominik Rebitzer <d.rebitzer@gmx.de>
2022-03-30 01:45:36 +0200

    update WiX assembly extension

--- WixExtensions/WixAssemblyInfoExtension/Properties/AssemblyInfo.cs
+++ WixExtensions/WixAssemblyInfoExtension/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: ComVisible(false)] 
