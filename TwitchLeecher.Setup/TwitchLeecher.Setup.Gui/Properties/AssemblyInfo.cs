c78d364b87628a22aa40eb5d70886c061ed69b99
Dominik Rebitzer <d.rebitzer@gmx.de>
2022-03-30 01:48:54 +0200

    CodeMaid pass

--- TwitchLeecher.Setup/TwitchLeecher.Setup.Gui/Properties/AssemblyInfo.cs
+++ TwitchLeecher.Setup/TwitchLeecher.Setup.Gui/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using TwitchLeecher.Setup.Gui;
 [assembly: AssemblyTitle("TwitchLeecher.Setup.Gui")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
-[assembly: BootstrapperApplication(typeof(SetupApplication))]
+[assembly: BootstrapperApplication(typeof(SetupApplication))] 
--- TwitchLeecher.Setup/TwitchLeecher.Setup.Publish/Properties/AssemblyInfo.cs
+++ TwitchLeecher.Setup/TwitchLeecher.Setup.Publish/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("TwitchLeecher.Setup.Publish")]
+[assembly: AssemblyTitle("TwitchLeecher.Setup.Publish")] 
